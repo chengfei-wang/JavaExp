c013cb962035d38402b520264fe2c3c594d5fc0b
wcf <1533144693@qq.com>
2019-11-20 10:03:17 +0800

    rm projs

--- 2019-2020(1)/JavaFinal/iChat_app/src/main/java/main/CONF.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public class CONF {
-    public static String APP_NAME = "iChat";
-    public static String CONF_FILE = "/app/"+APP_NAME+"/conf.dat";
-}
--- 2019-2020(1)/JavaFinal/iChat_server/src/main/java/model/Message.kt
+++
@@ -1,9 +0,0 @@
-package model
-
-import com.alibaba.fastjson.JSONObject
-
-class Message<T>(val code: Int, val msg: String, val data: T? = null) {
-    override fun toString(): String {
-        return JSONObject.toJSONString(this)
-    }
-}
