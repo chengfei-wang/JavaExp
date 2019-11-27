4846d8b07ae06332c1af9afd64a687ccb89861e8
wcf <1533144693@qq.com>
2019-11-27 21:11:21 +0800

    Update Item.kt

--- 2019-2020(1)/JavaFinal/PupilASMD/app/src/main/java/xyz/nfcv/pupil/asmd/ui/widget/Item.kt
+++ 2019-2020(1)/JavaFinal/PupilASMD/app/src/main/java/xyz/nfcv/pupil/asmd/ui/widget/Item.kt
@@ -1,3 +1,20 @@
 package xyz.nfcv.pupil.asmd.ui.widget
 
-class Item
+import androidx.compose.Composable
+import androidx.ui.foundation.Clickable
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.material.ripple.Ripple
+import androidx.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CreateExamOption() {
+    Ripple(bounded = false) {
+        Row {
+            Column {
+                
+            }
+        }
+    }
+}
